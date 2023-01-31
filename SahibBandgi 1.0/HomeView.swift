b0088c19f7cc81ac1fcb4272f95ba4724c2ba6bb
Sant Girdharanand Paramhansji Ashram <satgurumadhuparamhans@gmail.com>
2023-01-31 22:55:23 +0530

    Initial Commit

--- SahibBandgi 1.0/HomeView.swift
+++ SahibBandgi 1.0/HomeView.swift
@@ -29,7 +29,7 @@ struct HomeView: View {
                 //                    .background(Color.white)
                 
                 //                myColor.edgesIgnoringSafeArea(.all)
-                VStack(alignment: .leading) {
+                VStack {
                     HStack {
                         Button(action: {self.showingMenu.toggle()}) {
                             Image(systemName: "line.horizontal.3")
@@ -63,37 +63,35 @@ struct HomeView: View {
 
                     
                     //                    Slideshow
-                    //                    GeometryReader { proxy in
-                    TabView(selection: $currentIndex) {
-                        ForEach(2..<noOfImages) { num in
-                            Image("mobile_banner_"+"\(num)")
-                                .resizable()
-                                .scaledToFit()
-                            //                                    .padding(.top)
-                                .tag(num)
-                                .listRowBackground(myColor)
+//                    GeometryReader { proxy in
+                        TabView(selection: $currentIndex) {
+                            ForEach(2..<noOfImages) { num in
+                                Image("mobile_banner_"+"\(num)")
+                                    .resizable()
+//                                    .scaledToFill()
+                                //                                    .padding(.top)
+                                    .tag(num)
+                                    .listRowBackground(myColor)
+                            }
                         }
-                    }
-                    .tabViewStyle(PageTabViewStyle())
-                    //                            .clipShape(Rectangle)
-                    //                            .padding()
-                    .frame(width: 350)
-                    //                    .padding()
-                    //                        .frame(width: proxy.size.width * 0.95)
-                    //                        .scaledToFill()
-                    .onReceive(timer, perform: { _ in
-                        currentIndex = currentIndex < noOfImages ? currentIndex + 1 : 0
-                    })
-                    
-                    //                    }
+                        .tabViewStyle(PageTabViewStyle())
+                        //                            .clipShape(Rectangle)
+                        .padding(.top)
+                        .frame(width: 350, height: 450)
+//                        .frame(width: proxy.size.width, height: 475)
+//                        .scaledToFit()
+                        .onReceive(timer, perform: { _ in
+                            currentIndex = currentIndex < noOfImages ? currentIndex + 1 : 0
+                        })
+//                    }
                     
                     Text("Welcome! You are about to discover about the only sect in the world, which guarantees you TOTAL INNER PEACE and PERMANENT SALVATION. In today's world everyone is so focused on their body\'s needs, that they have forgotten the real reason for their birth as human beings, i.e. to attain salvation. This has lead to a life full of STRESS, PRESSURES, TENSIONS, DECEIT, ANGER, ANXIETY, LUST etc.\n\nSant Satguru Madhu Paramhans Sahib endearingly called as 'Sahib Ji' guides soul of an individual to follow the path of Satya Bhakti by blessing one's soul with the holy alive 'Naam' using His ULTIMATE SPIRITUAL POWERS. Then they attain complete INNER PEACE and PERMANENT SALVATION.\n\nEstablished in 1992, Sahib Bandgi Spiritual Organisation is purely a non-profit organisation with no affiliation whatsoever to any commercial or political organisation, around the world. Come and explore 'PURE SPIRITUALITY' and get rid of all your vices.")
                     //                        .foregroundColor(.pink)
-                        .font(.system(size: 18))
+                        .font(.system(size: 20))
                         .padding(15)
                         .background(lightPink)
+                        .minimumScaleFactor(0.05)
                     
-                    VStack {
                         Text("Satguru Madhu Paramhans")
                             .fontWeight(.heavy)
                             .font(.system(size: 20))
@@ -104,9 +102,10 @@ struct HomeView: View {
                             .cornerRadius(12)
                         
                         Text("His holiness Sadguru Madhu Paramhansji has served in the Indian army for 24 years. In 1967 at the age of 17, He joined Indian Army in Mahar Regiment and served the nation for 24 years till 1991. He retired as J.C.O (Junior Commissioned Officer) taking voluntary retirement from Indian Army. In 1970, at the age of 20, He started his journey towards awakening the souls with true knowledge of creation of the universe, purpose of its creation, how life was formed (jeevas), how different species were created, how they were bound by Kal Nirajan so that no jeeva can escape this creation and how humans can escape from this worldly ocean once they are blessed with 'Alive Holy Naam' (Saar Naam / Sajeevan Naam) through his spiritual discourses (Satsang). He started blessing the devotees with the real 'Saar Naam'. The only objective of Satguru is to liberate the souls of the Supreme Lord from the trap of the formless Mind (Nirakaar mann) and body (Maya) by making individual soul realise about the real motive and importance of human life. That's why He says: 'The thing that I possess, cannot be found anywhere in this Universe'.")
-                            .font(.system(size: 18))
+                            .font(.system(size: 20))
                             .padding(15)
                             .background(lightGrey)
+                            .minimumScaleFactor(0.05)
                         
                         
                         Text("Our Social Endavours")
@@ -114,7 +113,7 @@ struct HomeView: View {
                             .font(.system(size: 20))
                             .padding(15)
                         VideoView(videoID: "6kK6p8GLllU")
-                            .frame(minHeight: 0, maxHeight: UIScreen.main.bounds.height)
+                        .frame(minHeight: 0, maxHeight: UIScreen.main.bounds.height * 0.3)
                             .cornerRadius(12)
                         
                         //Slideshow
@@ -137,23 +136,21 @@ struct HomeView: View {
                         .onReceive(timer, perform: { _ in
                             currentIndex = currentIndex < noOfImages ? currentIndex + 1 : 0
                         })
-                        
-                        
+                    
+                    Text("---------Reach Us at----------")
+                        .font(.system(size: 18))
+                        .bold()
                         
                     }
-                    
+                HStack{
+                
                 }
+                
+                    
+//                }
 
                     
-                    //                    Text("This Is Home")
-                    //                        .font(
-                    //                            .largeTitle)
-                    //                        .foregroundColor(
-                    //                            .white)
-                    //                        .frame(maxWidth: .zero)
-                    //                    Spacer()
-                    //                }.padding(.horizontal) .frame(maxWidth: .infinity)
-//                }
+
                 //Floating Action Buttons
                 //
                 //                VStack {
